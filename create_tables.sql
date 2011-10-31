9eaec64aae745d55e2a85adade371f823dde0f6e
Frank Wang <frankw@cs.stanford.edu>
2011-10-30 22:43:34 -0700

    added database files

---
+++ create_tables.sql
@@ -0,0 +1,2 @@
+create table History(userid int, time string, bench_weight int, bench_reps int, biceps_weight int, biceps_reps int, pushups int, situps int, running_time int, running_dist int, time_spent int, primary key(userid, time));
+

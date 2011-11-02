2338e68feb02117e32280a75d84699505f2ae350
Kevin Ho <keho98@gmail.com>
2011-11-01 21:49:10 -0700

    Added points system

--- points.php
+++ points.php
@@ -2,7 +2,11 @@
 include('header.php');
 ?>
 
-Points explanation
+<h1>Points explanation</h1>
+<p>
+    For each weight based workout, Total Points = Reps X Weight.
+    For time based workouts, Total Points = 10 * Time (in minutes).
+</p>
 <br><br>
 <form action="log_confirm.php" method="post">
 <input type="submit" value="back" data-icon="back">

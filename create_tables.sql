fac4b129c75499d2bc41c061318088428e6407b5
Frank Wang <frankw@cs.stanford.edu>
2011-11-01 22:59:14 -0700

    added activity

--- create_tables.sql
+++ create_tables.sql
@@ -1,2 +1,2 @@
 create table activity (userid int, time string, type int, entry1 int, entry2 int, entry3 int, primary key(userid, time));
-
+create table username (userid int primary key, username string);

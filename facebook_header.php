8c3db98124241c5442b0eb0abfee8eb02454c460
wmu <wendymu@gmail.com>
2011-11-10 21:26:57 -0800

    remove log things

--- facebook_header.php
+++ facebook_header.php
@@ -2,14 +2,14 @@
 
 require 'fbphp/facebook.php';
 
-/*$facebook = new Facebook(array(
+$facebook = new Facebook(array(
   'appId'  => '193813890697848',
   'secret' => 'dd659bad86ad8c34d75a0e8eab304bbe',
 ));
-*/
+
 // See if there is a user from a cookie
-$user = 1;//$facebook->getUser();
-/*
+$user = $facebook->getUser();
+
 if ($user) {
   try {
     // Proceed knowing you have a logged in user who's authenticated.
@@ -17,6 +17,6 @@ if ($user) {
   } catch (FacebookApiException $e) {
     $user = null;
   }
-}*/
+}
 
 ?>

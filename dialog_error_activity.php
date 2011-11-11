409c71e2fc93e7b1f7586cbb0b25a4d20a487b70
wmu <wendymu@gmail.com>
2011-11-10 20:21:47 -0800

    adding files

---
+++ dialog_error_activity.php
@@ -0,0 +1,8 @@
+<?php
+include('header_all.php');
+?>
+<br>
+Please fill in current activity before adding a new one.
+<br><br>
+<a href="#" data-rel="back" data-role="button">OK</a>
+<?php include('footer_nonavbar.php'); ?>

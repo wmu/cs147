4fbbc5b4b7be7144d5759dd2b4c1ac9f10549755
Frank Wang <frankw@cs.stanford.edu>
2011-11-23 13:32:40 -0800

    changed time in database to integer

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,9 +1,9 @@
-insert into activity values(1057890118,'2011-10-05 10:01:00', 'bench', 8, 2, 0);
-insert into activity values(1057890118,'2011-10-07 10:00:01', 'biceps', 2, 3, 0);
-insert into activity values(624566123,'2011-10-06 09:00:00', 'other', 5, 2, 0);
-insert into activity values(624566123,'2011-10-03 10:00:24', 'pushups',10,0,0);
-insert into activity values(1063110054,'2011-10-04 20:00:12', 'running',2,10,0);
-insert into activity values(1063110054,'2011-10-10 14:00:53', 'bench', 10, 2, 0);
+insert into activity values(1057890118, 1317758412, 'bench', 8, 2, 0);
+insert into activity values(1057890118, 1317752000, 'biceps', 2, 3, 0);
+insert into activity values(624566123, 1317750412, 'other', 5, 2, 0);
+insert into activity values(624566123, 1317748412, 'pushups',10,0,0);
+insert into activity values(1063110054, 1317658212, 'running',2,10,0);
+insert into activity values(1063110054, 1317758212, 'bench', 10, 2, 0);
 insert into points values(1063110054, 320);
 insert into points values(624566123,110);
 insert into points values(1057890118,26);
